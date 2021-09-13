676161033342c652f679e0c75e255fc894f5f176
kirabboy <kirabboytt@gmail.com>
2021-09-13 11:02:24 +0700

    a

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -6,9 +6,3 @@ if (!function_exists('helper')) {
         return number_format($price, 0, '', ',');
     }
 }
-if (!function_exists('formatPrice')) {
-    function formatPrice($price)
-    {
-        return number_format($price, 0, '.', ',') . '₫';
-    }
-}
