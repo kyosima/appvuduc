58803c4fa2ab5c33a31ef4c3bdf76ddfd3b1ce33
kirabboy <kirabboytt@gmail.com>
2021-09-13 11:04:02 +0700

    d

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,11 +1,5 @@
-<?php 
+<?php
 
-if (!function_exists('helper')) {
-    function moneyFormat($price)
-    {
-        return number_format($price, 0, '', ',');
-    }
-}
 if (!function_exists('formatPrice')) {
     function formatPrice($price)
     {
